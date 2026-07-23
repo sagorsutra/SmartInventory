22d21d4fb10d7785afed2a0de95088ac864a5f09
Sagor <sagorsutra16@gmail.com>
2026-07-23 01:48:44 +0100

    debug enpoint removed

--- SmartInventory.Gateway/Program.cs
+++ SmartInventory.Gateway/Program.cs
@@ -5,23 +5,6 @@ builder.Services.AddReverseProxy()
 
 var app = builder.Build();
 
-// Debug endpoint - temporary
-app.MapGet("/debug-config", () =>
-{
-    var currentDir = Directory.GetCurrentDirectory();
-    var files = Directory.GetFiles(currentDir);
-    var appsettingsExists = File.Exists(Path.Combine(currentDir, "appsettings.json"));
-    string content = appsettingsExists ? File.ReadAllText(Path.Combine(currentDir, "appsettings.json")) : "FILE NOT FOUND";
-
-    return Results.Json(new
-    {
-        currentDir,
-        files,
-        appsettingsExists,
-        content
-    });
-});
-
 app.MapReverseProxy();
 
 app.Run();
